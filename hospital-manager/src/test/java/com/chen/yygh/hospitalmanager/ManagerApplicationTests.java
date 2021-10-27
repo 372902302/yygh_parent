b75cb2c2dc0b28731eeeb43acf9337f2925421c2
chensiyuan <372902302@qq.com>
2021-10-27 11:34:02 +0800

    升级spring版本。

--- hospital-manager/src/test/java/com/chen/yygh/hospitalmanager/ManagerApplicationTests.java
+++ hospital-manager/src/test/java/com/chen/yygh/hospitalmanager/ManagerApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HospitalManagerApplicationTests {
+class ManagerApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("test");
     }
 
 }
